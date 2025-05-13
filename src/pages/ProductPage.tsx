7c283e7bc32c1e11017876b06fdc5a413fbbaf4f
marned91 <152280051+marned91@users.noreply.github.com>
2025-05-13 14:27:10 +0200

    Adding toast notification for added to cart

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -18,6 +18,7 @@ export function ProductPage() {
   const { id } = useParams<{ id: string }>();
   const [product, setProduct] = useState<TProduct | null>(null);
   const [loading, setLoading] = useState(true);
+  const [added, setAdded] = useState(false);
 
   useEffect(() => {
     async function fetchProduct() {
@@ -35,31 +36,44 @@ export function ProductPage() {
   }, [id]);
 
   const addToCart = useCartStore((state) => state.addToCart);
+  const handleAddToCart = () => {
+    addToCart(product!);
+    setAdded(true);
+    setTimeout(() => setAdded(false), 2000);
+  };
 
   if (loading) return <p className="p-4">Loading product...</p>;
   if (!product) return <p className="p-4">Product not found.</p>;
 
   return (
-    <div className="bg-light flex flex-col-reverse md:flex-row-reverse justify-center py-20 px-5 md:gap-4">
-      <ProductImage
-        src={product.image.url}
-        alt={product.image.alt || product.title}
-      />
-      <div className="bg-white p-5 lg:p-10 rounded">
-        <ProductInfo
-          title={product.title}
-          description={product.description}
-          price={product.price}
-          discountedPrice={product.discountedPrice}
+    <div className="bg-light relative">
+      {added && (
+        <div className="absolute top-5 left-1/2 transform -translate-x-1/2 z-10">
+          <div className="bg-green-100 text-green-800 text-sm rounded-md px-4 py-2 shadow-md">
+            ✅ Product added to cart
+          </div>
+        </div>
+      )}
+      <div className=" flex flex-col-reverse md:flex-row-reverse justify-center py-20 px-5 md:gap-4">
+        <ProductImage
+          src={product.image.url}
+          alt={product.image.alt || product.title}
         />
-        <ProductReviews reviews={product.reviews} />
-
-        <button
-          onClick={() => addToCart(product)}
-          className="bg-primary text-white px-6 py-2 rounded font-semibold hover:bg-darkHover cursor-pointer"
-        >
-          Add to Cart
-        </button>
+        <div className="bg-white p-5 lg:p-10 rounded">
+          <ProductInfo
+            title={product.title}
+            description={product.description}
+            price={product.price}
+            discountedPrice={product.discountedPrice}
+          />
+          <ProductReviews reviews={product.reviews} />
+          <button
+            onClick={handleAddToCart}
+            className="bg-primary text-white px-6 py-2 rounded font-semibold hover:bg-darkHover cursor-pointer"
+          >
+            Add to Cart
+          </button>
+        </div>
       </div>
     </div>
   );
