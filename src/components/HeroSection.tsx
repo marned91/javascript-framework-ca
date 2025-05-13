40fe9cd04eb0f1a629b5f72e5f9f098d0732a3e5
marned91 <152280051+marned91@users.noreply.github.com>
2025-05-13 13:17:21 +0200

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -21,7 +21,7 @@ export function HeroSection({ onScrollClick }: props) {
     <div className="relative">
       <div className="absolute top-1/10 sm:top-1/3 left-1/2 transform -translate-x-1/2 text-center z-10 bg-white/50 p-8 rounded w-full sm:max-w-md mx-auto">
         <h1 className="text-xl md:text-3xl/12 font-large font-semibold">
-          Everything you need, in one place, just for YOU!
+          Shop smarter. Everything you need, just a click away!
         </h1>
         <button
           onClick={onScrollClick}
